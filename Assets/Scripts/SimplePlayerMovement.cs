60d470363f906c7800f944d021ac3470f56ab788
David LeGare <excaliburhissheath@gmail.com>
2014-01-25 09:40:02 -0600

    initial commit

---
+++ Assets/Scripts/SimplePlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimplePlayerMovement : MonoBehaviour {
+
+	public float movementSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rigidbody2D.AddForce (Vector2.right * movementSpeed);
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			rigidbody2D.AddForce(Vector2.right * -1 * movementSpeed);
+		}
+	}
+}
---
+++ Assets/Scripts/SimpleWorldGen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleWorldGen : MonoBehaviour
+{
+
+		public Transform platform;
+	public Transform player;
+		private List<Object> platforms = new List<Object> ();
+
+		// Use this for initialization
+		void Start ()
+		{
+				for (int i = -4; i <= 4; i++) {
+						platforms.Add (Instantiate (platform, new Vector3 (i, 0, 0), Quaternion.identity));
+				}
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				
+		}
+
+		void UpdateWorld ()
+		{
+				
+		}
+}

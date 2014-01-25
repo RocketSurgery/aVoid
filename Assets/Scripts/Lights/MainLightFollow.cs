d3570f08aed38cd16de67969110043b24172fd9e
Zac Gross <zac.a.gross@gmail.com>
2014-01-25 14:31:09 -0600

    Moving lights and static level

---
+++ Assets/Scripts/Lights/MainLightFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainLightFollow : MonoBehaviour {
+	
+	public GameObject mainCharacter;
+	public float beaconYOffset;
+	public float beaconXOffset;
+	
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(mainCharacter.transform.position.x + beaconXOffset, mainCharacter.transform.position.y + beaconYOffset, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Mathf.Abs (mainCharacter.transform.position.x - transform.position.x) < Mathf.Abs(beaconXOffset)) {
+			transform.position = new Vector3(mainCharacter.transform.position.x + beaconXOffset, mainCharacter.transform.position.y + beaconYOffset, 0);
+			//transform.position = mainCharacter.transform.position + new Vector3(beaconXOffset, beaconYOffset, 0);
+		}
+	}
+}

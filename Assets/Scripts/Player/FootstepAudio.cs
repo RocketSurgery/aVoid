70da43f1523455d8b96ab0faac78ea88955f5ce5
David LeGare <excaliburhissheath@gmail.com>
2014-01-26 04:58:18 -0600

    incrimental build

---
+++ Assets/Scripts/Player/FootstepAudio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FootstepAudio : MonoBehaviour
+{
+
+	public List<AudioClip> footStepSounds = new List<AudioClip> ();
+	public float soundDelayScalar = 1f;
+
+	void Awake ()
+	{
+		StartCoroutine (playFootstepSounds ());
+	}
+
+	public IEnumerator playFootstepSounds ()
+	{
+		CharacterController controller = GetComponent<CharacterController> ();
+
+		while (true) {
+			if (controller.isGrounded) {
+				audio.clip = footStepSounds [Random.Range (0, footStepSounds.Count)];
+				audio.Play ();
+			}
+			yield return new WaitForSeconds (controller.velocity.magnitude * soundDelayScalar);
+		}
+	}
+}

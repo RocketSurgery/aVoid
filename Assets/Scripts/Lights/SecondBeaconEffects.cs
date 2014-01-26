7c3e999cf387b54b1d18f662b257dc720a6a2706
Zac Gross <zac.a.gross@gmail.com>
2014-01-25 20:11:11 -0600

    background music layered in

---
+++ Assets/Scripts/Lights/SecondBeaconEffects.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondBeaconEffects : MonoBehaviour {
+	
+	public GameObject mainCharacter;
+	public Light pointLight;
+	public float intensityStart;
+	float scalar;
+	public float radius;
+	public float height;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pointLight.range = 2*Vector3.Distance(transform.position, mainCharacter.transform.position);
+		scalar = Vector3.Distance(transform.position, mainCharacter.transform.position) / 100;
+		pointLight.intensity = intensityStart * scalar;
+		transform.localScale = new Vector3(radius * scalar * .5f, height, radius * scalar * .5f);
+	}
+}
--- Assets/Scripts/Player/AtmosphereChanging.cs
+++ Assets/Scripts/Player/AtmosphereChanging.cs
@@ -6,18 +6,30 @@ public class AtmosphereChanging : MonoBehaviour {
 	public GameObject parent;
 	
 	public float firstCheckPoint;
-	public float SecondCheckPoint;
-	public float ThirdCheckPoint;
+	public float finalCheckPoint;
+	
+	public AudioSource[] musicLayers;
+	bool[] isPlaying;
 	
 	// Use this for initialization
 	void Start () {
+		isPlaying = new bool[musicLayers.Length];
+		musicLayers[0].Play();
+		isPlaying[0] = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		if (transform.position.x > firstCheckPoint) {
-				
+		for (int i = 1; i < 5; i++) {
+			if (!isPlaying[i] && transform.position.x > firstCheckPoint + i / 5.0 * (finalCheckPoint - firstCheckPoint))
+			{
+				musicLayers[i].PlayDelayed(15 - (Time.time % 15));
+				//musicLayers[i].Play();
+				isPlaying[i] = true;
+			}
 		}
+		
+
 	}
 }

241bc6b87cb4f827bf3d09684b3886c464f7993c
Zac Gross <zac.a.gross@gmail.com>
2014-01-26 08:32:06 -0600

    some final stuff

--- Assets/Scripts/World Generation/TileAlignmentReset.cs
+++ Assets/Scripts/World Generation/TileAlignmentReset.cs
@@ -11,9 +11,14 @@ public class TileAlignmentReset : MonoBehaviour
 		Debug.Log ("number of tiles: " + anchors.Length);
 
 		foreach(TileAlignmentAnchor anchor in anchors) {
-			if (anchor.gameObject.transform.position.y != 0f)
-				Debug.Log("old position: " + anchor.gameObject.transform.position);
-			anchor.gameObject.transform.position.Scale(new Vector3(1f, 0f, 1f));
+			Debug.Log("old position: " + anchor.gameObject.transform.position);
+			anchor.gameObject.transform.position.Set(Mathf.Round(anchor.gameObject.transform.position.x / 9.0f)*9,Mathf.Round(anchor.gameObject.transform.position.z / 9.0f)*9, 0);
+			
+			
+			
+			//if (anchor.gameObject.transform.position.y != 0f)
+			//	Debug.Log("old position: " + anchor.gameObject.transform.position);
+			//anchor.gameObject.transform.position.Scale(new Vector3(1f, 0f, 1f));
 //			Debug.Log("new position: " + anchor.gameObject.transform.position);
 		}
 	}
